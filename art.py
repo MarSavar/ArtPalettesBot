85aacf0854a22f253040cdaa1f19f0dcebddf0ad
msavar03 <msavar03@dcs.bbk.ac.uk>
2020-12-07 23:56:09 +0100

    Separated the logic and added docstrings.

--- art.py
+++ art.py
@@ -1,8 +1,4 @@
-import os
-import json
-import random
 import urllib.request
-import requests
 from art_functions import *
 from consts import *
 from sklearn.cluster import KMeans
@@ -10,18 +6,14 @@ from twython import *
 
 if __name__ == "__main__":
     # Pick a random work of art from Wikiart
-    page = random.randint(2500, 2976)
-    req = requests.get(f'{endpoint}?json=2&page={page}')
-    res = json.loads(req.text)
-    pick_one = random.randint(0, len(res['Paintings']) - 1)
-    work = res['Paintings'][pick_one]
+    work = get_random_work()
 
     # Parse details
-    title = work['title']
-    year = work['year']
-    artist_name = work['artistName']
-    image = image2 = work['image']
-    print(title, year, artist_name, image)
+    img = parse_details(work)
+    title = img['title']
+    artist_name = img['artist name']
+    year = img['year']
+    image = image2 = img['url']
 
     # Load image and convert to a list of pixels
     url = url_to_image(image)
@@ -30,21 +22,21 @@ if __name__ == "__main__":
 
     # Find and display most dominant colors
     cluster = KMeans(n_clusters=6).fit(reshape)
-    visualize, codes = visualize_colors(cluster, cluster.cluster_centers_)[0], \
-                       visualize_colors(cluster, cluster.cluster_centers_)[1]
+    visualize, codes = visualise_colours(cluster, cluster.cluster_centers_)[0], \
+                       visualise_colours(cluster, cluster.cluster_centers_)[1]
     visualize = cv2.cvtColor(visualize, cv2.COLOR_RGB2BGR)
     cv2.imwrite("tweet_palette.jpg", visualize)
 
+    # Authenticate ArtPalettesBot's main account
     twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
     photo = open('tweet_palette.jpg', 'rb')
     response = twitter.upload_media(media=photo)
     get_id = twitter.update_status(status=f'"{title}", {artist_name} ({year})\n\n'
                                           f'Hexadecimal values:\n{", ".join(codes)}.',
                                    media_ids=[response['media_id']])['id']
 
+    # Authenticate ArtPalettesBot's secondary account (comment)
     twitter = Twython(APP_KEY2, APP_SECRET2, OAUTH_TOKEN2, OAUTH_TOKEN_SECRET2)
-
     original = urllib.request.urlretrieve(image2, "tweet_original.jpg")
 
     with open('tweet_original.jpg', 'rb') as photo:
--- art_functions.py
+++ art_functions.py
@@ -1,14 +1,52 @@
 import numpy as np
 import urllib
 import cv2
+import random
+import requests
+import json
+from consts import *
 
 
 def clamp(x):
     return max(0, min(int(x), 255))
 
 
-def visualize_colors(cluster, centroids):
-    # Credits: colordetect.color_detect
+def get_random_work():
+    """
+    Select a random work of art from Wikiart.
+    Randint(x, y) selects a random page from the API.
+    """
+    page = random.randint(2500, 2976)
+    req = requests.get(f'{endpoint}?json=2&page={page}')
+    res = json.loads(req.text)
+    pick_one = random.randint(0, len(res['Paintings']) - 1)
+    return res['Paintings'][pick_one]
+
+
+def parse_details(work):
+    """
+    Take a work of art in JSON format as a parameter and parse keys and values.
+    """
+    title = work['title']
+    artist_name = work['artistName']
+    year = work['year']
+    url = work['image']
+
+    print(title, artist_name, year, url)
+
+    return {
+        'title': title,
+        'artist name': artist_name,
+        'year': year,
+        'url': url
+    }
+
+
+def visualise_colours(cluster, centroids):
+    """
+    Get the top six dominant colours and determine the width of each rectangle.
+    Credits: colordetect.color_detect
+    """
     labels = np.arange(0, len(np.unique(cluster.labels_)) + 1)
     (hist, _) = np.histogram(cluster.labels_, bins=labels)
     hist = hist.astype("float")
@@ -27,6 +65,9 @@ def visualize_colors(cluster, centroids):
 
 
 def url_to_image(url):
+    """
+    Convert url to image and continuoisly resize it if either the width or the height are greater than 1000.
+    """
     resp = urllib.request.urlopen(url)
     image = np.asarray(bytearray(resp.read()), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
